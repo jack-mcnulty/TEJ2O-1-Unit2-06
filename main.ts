b8ccb1cd545abf25d15b324502da1dc63600699b
jack-mcnulty <jack.mcnulty@stu.ocsb.ca>
2025-09-26 12:50:37 -0400

    turns on and off a LED

--- main.ts
+++ main.ts
@@ -1,8 +1,23 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Jack
+ * Created on: Sep 2025
+ * This program turns on and off an LED.
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+
+input.onButtonPressed(Button.A, function () {
+    // turns pin 16 on
+    basic.showIcon(IconNames.Yes)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+})
+
+
+input.onButtonPressed(Button.B, function () {
+    // turns pin 16 off
+    basic.showIcon(IconNames.No)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+})
